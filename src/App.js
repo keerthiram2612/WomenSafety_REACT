3a4b6fa1ac146f63c623acf51d505c6d2de90283
Keerthana <keerthiram2612@gmail.com>
2024-05-13 21:29:39 +0530

    creating and installing the router for defining the path

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react'
-import "./App.css";
+import "./index.css";
+import Home from "./routes/Home"
+import About from "./routes/About"
+import Admin from "./routes/Admin"
+import Contact from "./routes/Contact"
+import {Routes,Route}from"react-router-dom"
 import Navbar from './Components/Navbar';
-
+import SignIn from "./routes/SigIn"
 export default function App() {
   return (
-    <div className='App'>
-     <Navbar/>
-      
-    </div>
+    <>
+    <Navbar/>
+    <Routes>
+     <Route path = "/" element = {<Home/>}/>
+     <Route path = "/about" element = {<About/>}/>
+     <Route path = "/admin" element = {<Admin/>}/>
+     <Route path = "/contact" element = {<Contact/>}/>
+     <Route path = "/signin" element = {<SignIn/>}/>
+    </Routes>
+    </>
   )
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,22 +1,26 @@
 import React from 'react'
 import "./Navbar.css"
+import { Link } from 'react-router-dom'
 export default function Navbar() {
   return (
     <div className='header'>
-   
+     
         <h1>Wosafe</h1>
       <ul className='nav-menu'>
         <li>
-            Home
+            <Link to ="/">Home</Link>
         </li>
         <li>
-            About
+        <Link to ="/about">About</Link>
         </li>
         <li>
-            Admin
+          <Link to = "/admin">Admin</Link>
         </li>
         <li>
-            Contact
+        <Link to ="/contact">Contact</Link>
+        </li>
+        <li>
+        <Link to = "/signin">SigIn</Link>
         </li>
       </ul>
     </div>
