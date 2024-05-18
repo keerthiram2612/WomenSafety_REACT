e2af5fe0efdf3e93c0aca90a7b8e79b4bff62fc8
Keerthana <keerthiram2612@gmail.com>
2024-05-18 20:35:32 +0530

    fixing the image to the home page

--- src/Components/HeroImg.js
+++ src/Components/HeroImg.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import "./HomeContentstyle.css"
+import WomenPic from "../assets/womensafetypic.png"
 export default function HeroImg() {
   return (
     <div className='home'>
+      <div className='image-home'>
+       <img className='women-pic'src={WomenPic} alt='WomenPic'/>
+      </div>
       <h1>WomenSafety</h1>
     </div>
   )
