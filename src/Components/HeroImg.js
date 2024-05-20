161fd221c3d7a33f7e03e2c1a2a30bc1f7cea250
Keerthana <keerthiram2612@gmail.com>
2024-05-20 20:58:07 +0530

    adding the usestate and creating the button

--- src/Components/HeroImg.js
+++ src/Components/HeroImg.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./HomeContentstyle.css"
 import WomenPic from "../assets/womensafetypic.png"
 export default function HeroImg() {
+  const[show,setShow]= useState(true);
   return (
     <div className='home'>
       <div className='image-home'>
        <img className='women-pic'src={WomenPic} alt='WomenPic'/>
       </div>
-      <h1>WomenSafety</h1>
+      <div className='container'>
+      <h1 className='home-h1'>WomenSafety</h1><br/>
+      <button onClick={()=>setShow((show)=>!show)}>Show/hide</button>
+      {show?  <p className='home-p'>This application tracks and gives the suggestion for the people which is the right 
+        place and gives the notification in critical situation by the current location.
+      </p>:null}
+     
+      </div>
     </div>
   )
 }
