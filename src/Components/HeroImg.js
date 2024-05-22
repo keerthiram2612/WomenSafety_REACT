aa09390aa699f8d6e1677904253ba258ad9e23e4
Keerthana <keerthiram2612@gmail.com>
2024-05-22 21:06:27 +0530

    adding the location in the home page

--- src/Components/HeroImg.js
+++ src/Components/HeroImg.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react'
 import "./HomeContentstyle.css"
 import WomenPic from "../assets/womensafetypic.png"
 export default function HeroImg() {
+  const handleClick=()=>{
+    window.location.href="https://www.google.com/maps/@13.0925053,80.2432831,15z?entry=ttu"
+  }
   const[show,setShow]= useState(true);
   return (
     <div className='home'>
@@ -10,11 +13,10 @@ export default function HeroImg() {
       </div>
       <div className='container'>
       <h1 className='home-h1'>WomenSafety</h1><br/>
-      <button onClick={()=>setShow((show)=>!show)}>Show/hide</button>
-      {show?  <p className='home-p'>This application tracks and gives the suggestion for the people which is the right 
+      <p className='home-p'>This application tracks and gives the suggestion for the people which is the right 
         place and gives the notification in critical situation by the current location.
-      </p>:null}
-     
+      </p>
+      <button onClick={handleClick}>Location</button>
       </div>
     </div>
   )
