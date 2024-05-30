2a70766a82c88f218862b150f594c32d538057fb
Keerthana <keerthiram2612@gmail.com>
2024-05-30 21:10:09 +0530

    creating the footer design

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import "./Footerstyle.css"
 export default function Footer() {
   return (
-    <div>
+    <div className='footer'>
+      <div className='footer-container'>
+        <h2>Copyright@2024</h2>
+      </div>
       
     </div>
   )
