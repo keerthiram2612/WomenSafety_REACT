9bc248553843c0ef5a7e299879b364a05352646d
Keerthana <keerthiram2612@gmail.com>
2024-07-11 20:06:15 +0530

    creating the list by defining the usestate

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,13 +1,15 @@
 import React, { useState } from 'react'
 import "./Navbar.css"
 import { Link } from 'react-router-dom'
-import { FaBars, FaTimes } from 'react-icons/fa'
+import { FaArrowDown, FaArrowRight, FaArrowUp, FaBars, FaTimes } from 'react-icons/fa'
 
 export default function Navbar() {
   const [click,setClick]=useState(false);
   const handleClick = () => setClick(!click);
   const[color,setColor]=useState(false)
-
+  const [isOpen ,setIsOpen]  = useState (false);
+    const handledown = () => setIsOpen (!isOpen);
+  
   const changeColor = () =>{
     if(window.scrollY>=100){
       setColor(true)
@@ -19,7 +21,7 @@ export default function Navbar() {
   return (
     <div className={color ? "header header-bg":"header"}>
         <Link to = "/">
-        <h1>Wo-safe</h1>
+        <h1 className='logo'>Wo-safe</h1>
         </Link>
       <ul className={click?"nav-menu active":"nav-menu"}>
         <li>
@@ -29,7 +31,24 @@ export default function Navbar() {
         <Link to ="/about">About</Link>
         </li>
         <li>
-          <Link to = "/complaints">Complaints</Link>
+          <Link to = "/complaints">Complaints 
+          </Link>  
+            <div onClick={handledown} className='list'>
+            {isOpen ? (<FaArrowDown style={{width:"40px",color:"white",
+              marginLeft: "106px",
+              transform:"translateY(-20px)"
+            }}/>):(
+             <FaArrowUp  style={{
+              width:"40px",color:"white",
+              marginLeft: "110px",
+              transform:"translateY(-20px)"}}
+/>            )}
+           <ul className={isOpen ?'nav-down active ':'navdown' }>
+              <li>Complaints</li><br/>
+              <li>Complaints</li><br/>
+              <li>Complaints</li><br/>
+            </ul>
+            </div>
         </li>
         <li>
         <Link to ="/contact">Contact</Link>
---
+++ frontend/src/Components/Slider.js
@@ -0,0 +1,13 @@
+import  "./Slider.css"
+
+import React from 'react'
+
+const Slider = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Slider
